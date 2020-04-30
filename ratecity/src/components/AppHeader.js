844a9af504eccc567f75cc974b4ad1f0fb6fab3c
Yatin Kalra <yatin.kalra22@gmail.com>
2020-05-01 01:31:01 +0530

    added header text with some css for responiveness

--- ratecity/src/components/AppHeader.js
+++ ratecity/src/components/AppHeader.js
@@ -3,6 +3,20 @@ import "../styles/AppHeader.css";
 
 export default class AppHeader extends Component {
   render() {
-    return <div>Header of the application</div>;
+    return (
+      <div className="App-header-container">
+        <div className="headline-text" id="top-home-loan-testcase">
+          Top Home Loan Products
+        </div>
+        <div className="headline-menu">
+          <div id="all-testcase">ALL</div>
+          <div id="refinance-testcase">REFINANCE</div>
+          <div id="fixed rate-testcase">FIXED RATE</div>
+          <div id="first-home-buyer-testcase">FIRST HOME BUYER</div>
+          <div id="nvestor-testcase">NVESTOR</div>
+          <div id="next-home-buyer-testcase">NEXT HOME BUYER</div>
+        </div>
+      </div>
+    );
   }
 }
