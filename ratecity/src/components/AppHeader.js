d6dc74cc17f78a2fadf6c24282f2fe65c599f467
Yatin Kalra <yatin.kalra22@gmail.com>
2020-05-01 01:41:15 +0530

    added test cases for header text

--- ratecity/src/components/AppHeader.js
+++ ratecity/src/components/AppHeader.js
@@ -11,7 +11,7 @@ export default class AppHeader extends Component {
         <div className="headline-menu">
           <div id="all-testcase">ALL</div>
           <div id="refinance-testcase">REFINANCE</div>
-          <div id="fixed rate-testcase">FIXED RATE</div>
+          <div id="fixed-rate-testcase">FIXED RATE</div>
           <div id="first-home-buyer-testcase">FIRST HOME BUYER</div>
           <div id="nvestor-testcase">NVESTOR</div>
           <div id="next-home-buyer-testcase">NEXT HOME BUYER</div>
--- ratecity/src/components/__test__/AppHeader.test.js
+++ ratecity/src/components/__test__/AppHeader.test.js
@@ -1,3 +1,28 @@
+import React from "react";
+import { shallow } from "enzyme";
+import AppHeader from "../../components/AppHeader";
+
+const wrapper = shallow(<AppHeader />);
+
 describe("AppHeader Component", () => {
-  it("should ", () => {});
+  it("should render the component", () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("should have 'Top Home Loan Products' as the headerText ", () => {
+    expect(wrapper.find("#top-home-loan-testcase").text()).toBe(
+      "Top Home Loan Products"
+    );
+  });
+  it("should have multiple Text in the headLine menu ", () => {
+    expect(wrapper.find("#all-testcase").text()).toBe("ALL");
+    expect(wrapper.find("#refinance-testcase").text()).toBe("REFINANCE");
+    expect(wrapper.find("#fixed-rate-testcase").text()).toBe("FIXED RATE");
+    expect(wrapper.find("#first-home-buyer-testcase").text()).toBe(
+      "FIRST HOME BUYER"
+    );
+    expect(wrapper.find("#nvestor-testcase").text()).toBe("NVESTOR");
+    expect(wrapper.find("#next-home-buyer-testcase").text()).toBe(
+      "NEXT HOME BUYER"
+    );
+  });
 });
