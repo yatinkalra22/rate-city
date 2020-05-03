3ffff3ab805fda1db063b3024a266888d1ee9fba
Yatin Kalra <yatin.kalra22@gmail.com>
2020-05-03 15:57:46 +0530

    visualization xheckbox and interest input  added

--- ratecity/src/components/LoanProductList.js
+++ ratecity/src/components/LoanProductList.js
@@ -5,14 +5,17 @@ import rightArrow from "../assests/images/right-arrow.png";
 import { connect } from "react-redux";
 import Spinner from "./Spinner";
 import { getHomeLoanProducts } from "../actions/HomeLoanProducts";
+import VisualizationGraph from "./VisualizationGraph";
 
 export class LoanProductList extends Component {
   state = {
     comparedCheckBox: [],
     compareCheckBoxNumber: 3,
+    visualizationCheckBox: false,
     currentPage: 1,
     maximumPage: 4,
     minimumPage: 1,
+    interestRate: "",
   };
 
   // calling the api to fetch first page
@@ -43,7 +46,7 @@ export class LoanProductList extends Component {
   };
 
   // when compare button is pressed then, then checkbox is set to be true, only for 3 check box will be selected.
-  toggleChange = (uuid) => {
+  toggleCompareChange = (uuid) => {
     const { comparedCheckBox, compareCheckBoxNumber } = this.state;
     let newComparedCheckBox = comparedCheckBox;
     if (!newComparedCheckBox.includes(uuid)) {
@@ -63,8 +66,20 @@ export class LoanProductList extends Component {
     }
   };
 
+  toggleVisualizationChange = () => {
+    this.setState({
+      visualizationCheckBox: !this.state.visualizationCheckBox,
+    });
+  };
+
+  onChange = (event) => {
+    if (event.target.value >= 0 && event.target.value <= 100) {
+      this.setState({ [event.target.name]: event.target.value });
+    }
+  };
+
   render() {
-    const { comparedCheckBox, currentPage } = this.state;
+    const { comparedCheckBox, currentPage, visualizationCheckBox } = this.state;
     const { homeloan_products, selectedMenuOption } = this.props;
     // api response is filtered based on the menu selected item
     let fliteredHomeLoanList = [];
@@ -85,34 +100,69 @@ export class LoanProductList extends Component {
         {/* Pagination Code */}
         {/* If there is any products list then only showing the pagination */}
         {fliteredHomeLoanList.length > 0 ? (
-          <div className="pagintion-container">
-            {/* If showing first page then previous button will be faded/text */}
-            {currentPage > 1 ? (
-              <div
-                onClick={this.decrementPageNumber}
-                className="pagination-button"
-              >
-                Previous
+          <div className="pagination-graph-container">
+            <div className="pagintion-container">
+              {/* If showing first page then previous button will be faded/text */}
+              {currentPage > 1 ? (
+                <div
+                  onClick={this.decrementPageNumber}
+                  className="pagination-button"
+                >
+                  Previous
+                </div>
+              ) : (
+                <span className="faded-text">Previous</span>
+              )}
+              {/* showing the current page */}
+              <span style={{ padding: "0 10px", margin: "5px 5px" }}>
+                {currentPage} of 4
+              </span>
+              {/* If showing last page then Next button will be faded/text */}
+              {currentPage < 4 ? (
+                <div
+                  onClick={this.incrementPageNumber}
+                  className="pagination-button"
+                >
+                  Next
+                </div>
+              ) : (
+                <span className="faded-text">Next</span>
+              )}
+            </div>
+            {/* Visualization option */}
+            <div className="visualization-container">
+              <div> Visualizatin</div>
+              <input
+                className="visualization-checkbox"
+                type="checkbox"
+                checked={visualizationCheckBox}
+                onChange={this.toggleVisualizationChange}
+              />
+            </div>
+          </div>
+        ) : null}
+        {/* Graph of the application */}
+        {visualizationCheckBox ? (
+          <div className="graph-container">
+            <div>
+              <div style={{ display: "flex" }}>
+                <input
+                  className="interst-rate-input"
+                  type="text"
+                  placeholder="Enter your interest rate"
+                  onChange={this.onChange}
+                  name="interestRate"
+                  value={this.state.interestRate}
+                />
+                <div
+                  // onClick={this.incrementPageNumber}
+                  className="search-button"
+                >
+                  Compare
+                </div>
               </div>
-            ) : (
-              <span className="faded-text">Previous</span>
-            )}
-            {/* showing the current page */}
-            <span style={{ padding: "0 10px", margin: "5px 5px" }}>
-              {currentPage} of 4
-            </span>
-            {/* If showing last page then Next button will be faded/text */}
-            {currentPage < 4 ? (
-              <div
-                onClick={this.incrementPageNumber}
-                className="pagination-button"
-              >
-                Next
-              </div>
-            ) : (
-              <span className="faded-text">Next</span>
-            )}
-            <div>Visualization</div>
+              <VisualizationGraph />
+            </div>
           </div>
         ) : null}
         {/* if api is called then showing the loader */}
@@ -168,7 +218,9 @@ export class LoanProductList extends Component {
                                 )
                               : false
                           }
-                          onChange={() => this.toggleChange(each_product.uuid)}
+                          onChange={() =>
+                            this.toggleCompareChange(each_product.uuid)
+                          }
                           className="checkbox"
                         />
                         <div>Compare</div>
---
+++ ratecity/src/components/VisualizationGraph.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import { Line } from "react-chartjs-2";
+
+export default class VisualizationGraph extends Component {
+  render() {
+    return <div>Hello</div>;
+  }
+}
