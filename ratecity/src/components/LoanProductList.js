68be0a57b57a39eb55f3f6c16e929b3b95b39344
Yatin Kalra <yatin.kalra22@gmail.com>
2020-05-03 17:23:55 +0530

    visualization completed

--- ratecity/src/components/LoanProductList.js
+++ ratecity/src/components/LoanProductList.js
@@ -79,7 +79,12 @@ export class LoanProductList extends Component {
   };
 
   render() {
-    const { comparedCheckBox, currentPage, visualizationCheckBox } = this.state;
+    const {
+      comparedCheckBox,
+      currentPage,
+      visualizationCheckBox,
+      interestRate,
+    } = this.state;
     const { homeloan_products, selectedMenuOption } = this.props;
     // api response is filtered based on the menu selected item
     let fliteredHomeLoanList = [];
@@ -100,71 +105,79 @@ export class LoanProductList extends Component {
         {/* Pagination Code */}
         {/* If there is any products list then only showing the pagination */}
         {fliteredHomeLoanList.length > 0 ? (
-          <div className="pagination-graph-container">
-            <div className="pagintion-container">
-              {/* If showing first page then previous button will be faded/text */}
-              {currentPage > 1 ? (
-                <div
-                  onClick={this.decrementPageNumber}
-                  className="pagination-button"
-                >
-                  Previous
-                </div>
-              ) : (
-                <span className="faded-text">Previous</span>
-              )}
-              {/* showing the current page */}
-              <span style={{ padding: "0 10px", margin: "5px 5px" }}>
-                {currentPage} of 4
-              </span>
-              {/* If showing last page then Next button will be faded/text */}
-              {currentPage < 4 ? (
-                <div
-                  onClick={this.incrementPageNumber}
-                  className="pagination-button"
-                >
-                  Next
-                </div>
-              ) : (
-                <span className="faded-text">Next</span>
-              )}
-            </div>
-            {/* Visualization option */}
-            <div className="visualization-container">
-              <div> Visualizatin</div>
-              <input
-                className="visualization-checkbox"
-                type="checkbox"
-                checked={visualizationCheckBox}
-                onChange={this.toggleVisualizationChange}
-              />
-            </div>
-          </div>
-        ) : null}
-        {/* Graph of the application */}
-        {visualizationCheckBox ? (
-          <div className="graph-container">
-            <div>
-              <div style={{ display: "flex" }}>
+          <>
+            <div className="pagination-graph-container">
+              <div className="pagintion-container">
+                {/* If showing first page then previous button will be faded/text */}
+                {currentPage > 1 ? (
+                  <div
+                    onClick={this.decrementPageNumber}
+                    className="pagination-button"
+                  >
+                    Previous
+                  </div>
+                ) : (
+                  <span className="faded-text">Previous</span>
+                )}
+                {/* showing the current page */}
+                <span style={{ padding: "0 10px", margin: "5px 5px" }}>
+                  {currentPage} of 4
+                </span>
+                {/* If showing last page then Next button will be faded/text */}
+                {currentPage < 4 ? (
+                  <div
+                    onClick={this.incrementPageNumber}
+                    className="pagination-button"
+                  >
+                    Next
+                  </div>
+                ) : (
+                  <span className="faded-text">Next</span>
+                )}
+              </div>
+              {/* Visualization option */}
+              <div className="visualization-container">
+                <div> Visualizatin</div>
                 <input
-                  className="interst-rate-input"
-                  type="text"
-                  placeholder="Enter your interest rate"
-                  onChange={this.onChange}
-                  name="interestRate"
-                  value={this.state.interestRate}
+                  className="visualization-checkbox"
+                  type="checkbox"
+                  checked={visualizationCheckBox}
+                  onChange={this.toggleVisualizationChange}
                 />
-                <div
-                  // onClick={this.incrementPageNumber}
-                  className="search-button"
-                >
-                  Compare
+              </div>
+            </div>
+            {/* Graph of the application */}
+            {visualizationCheckBox ? (
+              <div className="graph-container">
+                <div>
+                  <div style={{ display: "flex", justifyContent: "center" }}>
+                    <input
+                      className="interst-rate-input"
+                      type="text"
+                      placeholder="Enter your interest rate"
+                      onChange={this.onChange}
+                      name="interestRate"
+                      value={this.state.interestRate}
+                    />
+                    <div
+                      // onClick={this.incrementPageNumber}
+                      className="search-button"
+                    >
+                      Compare
+                    </div>
+                  </div>
+                  {this.props.isloading ? null : (
+                    <VisualizationGraph
+                      fliteredHomeLoanList={fliteredHomeLoanList}
+                      interestRate={interestRate}
+                    />
+                  )}
                 </div>
               </div>
-              <VisualizationGraph />
-            </div>
-          </div>
+            ) : null}
+          </>
         ) : null}
+
         {/* if api is called then showing the loader */}
         {this.props.isloading ? (
           <Spinner />
--- ratecity/src/components/VisualizationGraph.js
+++ ratecity/src/components/VisualizationGraph.js
@@ -1,8 +1,95 @@
 import React, { Component } from "react";
 import { Line } from "react-chartjs-2";
+import "../styles/VisualizationGraph.css";
 
 export default class VisualizationGraph extends Component {
   render() {
-    return <div>Hello</div>;
+    let data = { labels: [], comparisonRate: [], colors: [] };
+    for (let i = 0; i < this.props.fliteredHomeLoanList.length; i++) {
+      data.labels.push(this.props.fliteredHomeLoanList[i].company.name);
+      data.comparisonRate.push(
+        this.props.fliteredHomeLoanList[i].comparisonRate
+      );
+      if (this.props.interestRate) {
+        if (
+          this.props.interestRate >
+          this.props.fliteredHomeLoanList[i].comparisonRate
+        ) {
+          data.colors.push("red");
+        } else if (
+          this.props.interestRate <
+          this.props.fliteredHomeLoanList[i].comparisonRate
+        ) {
+          data.colors.push("green");
+        } else {
+          data.colors.push("blue");
+        }
+      }
+    }
+
+    return (
+      <div
+        className="visualization-graph"
+        style={{ height: "40vh", width: "90vw" }}
+      >
+        <Line
+          data={{
+            labels: data.labels ? data.labels : "",
+            datasets: [
+              {
+                label: "Comparison interest rate",
+                data: data ? data.comparisonRate : "",
+                fill: false,
+                borderColor: "#5B83C3",
+                borderWidth: 2,
+                pointBackgroundColor: data ? data.colors : "#4adbc8",
+                pointRadius: 8,
+              },
+            ],
+          }}
+          options={{
+            maintainAspectRatio: false,
+            scales: {
+              xAxes: [
+                {
+                  scaleLabel: {
+                    display: true,
+                    labelString: "Bank Names",
+                  },
+                  gridLines: {
+                    display: false,
+                  },
+                },
+              ],
+              yAxes: [
+                {
+                  // scaleStepWidth: 50,
+                  scaleLabel: {
+                    display: true,
+                    labelString: "Comparison Rate",
+                  },
+                  gridLines: {
+                    display: true,
+                    drawBorder: false,
+                    color: "#c4c4c4",
+                  },
+                },
+              ],
+            },
+            legend: {
+              display: true,
+              position: "top",
+              align: "end",
+              labels: {
+                fontColor: "#33333",
+
+                boxWidth: 10,
+                padding: 20,
+              },
+            },
+          }}
+        />
+      </div>
+    );
   }
 }
